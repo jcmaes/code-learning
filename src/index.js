c1363cf459ba44ffac792a605c4196cc11b40ef2
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-11-28 21:59:45 +0100

    Lesson 29 ending.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-/* Numbers */
+/* Math */
 
-const a = 1.5555555555;
-const b = new Number(1);
+const a = Math.pow(2, 3);
+console.log(a) // 8
 
-console.log(a);
-console.log(b);
+const b = Math.PI
+console.log(b) // 3.141592653589793
 
-console.log(a.toFixed(2)); // 1.56
-console.log(a.toString(2)); // 1.56
+const c = Math.ceil(1.01);
+console.log(c) // 2
 
-const c = Number("123");
-console.log(Number.isNaN(c)); // false
+const d = Math.floor(1.9);
+console.log(d) // 1
 
-const d = Number("55.1");
-console.log(Number.isInteger(d)); // false
+const e = Math.round(2.4);
+console.log(e) // 2
+
+const f = Math.max(10, 8, 5, 55, 66);
+console.log(f) // 66
+
+const g = Math.max(10, 8, 5, 55, 66);
+console.log(g) // 5
+
+const h = Math.random();
+console.log(h) // 0.1674179021302853
+
+const i = Math.round(Math.random() * 100);
+console.log(i) // 73
+
+const j = Math.round((Math.random() * 10) % 5);
+console.log(j) // 3
