eb6a50743525b19c64bbf67b9b40c087d4d4e8c1
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-11-28 18:27:23 +0100

    Lesson 24 ending.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-/* Loops and iteration: For */
+/* Loops and iteration: While */
+let i = 55;
 
-for (let i = 0; i < 3; i++) {
-    if (i === 1) {
-        break;
-    }
-    if (i === 2) {
-        continue;
-    }
+do {
     console.log(i);
+    i ++;
+} while ( i < 10) {
+    console.log(i);
+    i++;
 }
 
 console.log("out");
