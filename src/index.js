6d9c610a1687112615ed5d3a1589e4a8e9c09b46
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-11-29 19:16:43 +0100

    Lesson 32 ending.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-/* String literals */
+/* The index and length properties */
 
-const a = "one";
-const b = "two";
-const c = "I\'m a string ${a}";
-console.log(c);
+const a = "earth";
+
+console.log(a[0]); // e
+console.log(a[1]); // a
+console.log(a[2]); // r
+console.log(a[3]); // t
+console.log(a[4]); // h
+console.log(a[5]); // undefined
+
+console.log(a.length); // 5
+
+console.log(a[a.length - 1]); // h
+
+const b = new String("earth");
+console.log(b);
