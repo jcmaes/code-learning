e5ad9f0a6de4ce7860f700ff11aff7f70b055013
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-11-29 20:21:19 +0100

    Lesson 34 ending.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
-/* Methods available on strings */
+/* Regular expressions */
 
-const a = "Sun";
-console.log(a);
+const a = "This is the 645sun";
+// const evaluate = /sun/.test(a); // true
+// const evaluate = /\d{1,3}s/.test(a); // true
+// const evaluate = /\Ds/.test(a); // true
+// const evaluate = / .\d{1,3}/.test(a); // true
+// const evaluate = /[le]\d/.test(a); // false
+// const evaluate = /[^le] \d/.test(a); // false
+// const evaluate = /[a-z]/.test(a); // true
+const evaluate = /\d{3}[A-Z]?[a-z]/.test(a); // true
+console.log(evaluate);
 
-const b = a.charAt(0);
-console.log(b); // S
+const b = "picture_13456.jpg";
+const newEvaluate = /(.*)\./.exec(b);
+console.log(newEvaluate);
 
-const c = "I\'m the sun";
-const d = c.endsWith('sun');
-console.log(d); // true
-
-const e = c.startsWith('sun');
-console.log(e); // false
-
-const f = c.indexOf('sun');
-console.log(f); // 8
-
-const g = c.indexOf('earth');
-console.log(g); // -1
-
-const h = c.replace('sun', 'moon');
-console.log(h); // I'm the moon
-
-const i = c.search('sun');
-console.log(i); // 8
-
-const j = c.slice(2, -1);
-console.log(j); // m the su
-
-const k = c.trim();
-console.log(k); // I'm the sun
-
-const l = "sun, earth, moon";
-const m = l.split(",");
-console.log(m);
+b.match(/.*\./);
+b.split(/.*\./);
