76644f4d9b7659e91e4203fce2fbd1f7488a925b
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-11-30 15:37:58 +0100

    Lesson 35 ending.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-/* Regular expressions */
+/* Object */
 
-const a = "This is the 645sun";
-// const evaluate = /sun/.test(a); // true
-// const evaluate = /\d{1,3}s/.test(a); // true
-// const evaluate = /\Ds/.test(a); // true
-// const evaluate = / .\d{1,3}/.test(a); // true
-// const evaluate = /[le]\d/.test(a); // false
-// const evaluate = /[^le] \d/.test(a); // false
-// const evaluate = /[a-z]/.test(a); // true
-const evaluate = /\d{3}[A-Z]?[a-z]/.test(a); // true
-console.log(evaluate);
+const a = {}; // literal
+console.log(a);
 
-const b = "picture_13456.jpg";
-const newEvaluate = /(.*)\./.exec(b);
-console.log(newEvaluate);
+const b = Object(); // object
+console.log(b);
 
-b.match(/.*\./);
-b.split(/.*\./);
+const c = new Object(); // constructor object
+console.log(c);
