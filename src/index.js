550e6dce62d4072243b865ea67a272fd0bed63ff
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-12-03 14:52:01 +0100

    Lesson 39 ending.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-/* Object properties */
-
-const population = 50;
+/* Test the existence and value of a property */
 
 const earth = {
     population: 7e7,
@@ -12,7 +10,23 @@ const earth = {
     isOld: false,
 };
 
-const { population: populationEarth, satellite, temperature, ...rest } = earth;
+// Test 1
+if (earth.population) {
+    console.log("Ok !");
+} else {
+    console.log("Nok !");
+}
 
-console.log(population, populationEarth, satellite, temperature);
-console.log(rest);
+// Test 2
+if ("population" in earth) {
+    console.log("Ok !");
+} else {
+    console.log("Nok !");
+}
+
+// Test 3
+if (earth.hasOwnProperty("test")) {
+    console.log("Ok !");
+} else {
+    console.log("Nok !");
+}
