9a5bd19f548fdf98d7dd0a3f7661c00a22ed8040
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-12-06 21:26:18 +0100

    Lesson 51 ending.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-/* Lexical environment and execution context */
+/* The stock chain */
 
 // Example 1
-function a() {}
+function a() {
+    console.log(foo);
+}
 function b() {
+    let foo = 1;
     a();
 }
+
+let foo = 2;
 b();
 
 // Example 2
-function c() {
-    let text;
-}
 function d() {
+    function c() {
+        console.log(bar);
+    }
+    let bar = 1;
     c();
-    let foo;
 }
+
+let bar = 2;
 d();
