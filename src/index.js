824350a3180dd2dc8eaa694b7b4766cc9a5f8acf
Jean-Christophe Maes <92582027+jcmaes@users.noreply.github.com>
2022-12-06 20:40:21 +0100

    Lesson 48 ending.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-/* Parameters, arguments and default settings */
+/* Object arguments and use of the Rest operator */
 
-// Declaration of a function
-function func(param = "default") {
-    // param = param || "default"; // Old way
-    if (param) {
-        console.log(param.toUpperCase());
+// Example 1
+function add() {
+    let total = 0;
+    for (let i = 0;i< arguments.length;i++) {
+        total += arguments[i];
+    }
+    console.log(total);
+}
+
+add(1, 2, 3, 4);
+
+// Example 2
+function operation(operator, ...numbers) {
+    let newTotal = 0;
+    if (operator === "+") {
+        for (let i = 0;i< numbers.length;i++) {
+            newTotal += numbers[i];
+        }
+        console.log(newTotal);
     }
 }
-func("param"); // PARAM
-func(); // DEFAULT
+
+operation("+", 1, 2, 3, 4);
