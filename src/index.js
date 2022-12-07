4401e8f54816c20e8e73283daf26a2015f14e7a9
Jean-Christophe Maes <jc.maes@wavenet.be>
2022-12-07 16:56:38 +0100

    Lesson 55 ending.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,59 @@
-/* Set or link this */
+/* Arrow functions */
 
-const a = {
-    foo: "not bar"
+// Example 1
+const a = (param) => {
+    console.log(param);
+}
+
+a(); // Undefined
+a("foo"); // foo
+
+// Example 2
+const b = (c, d) => c + d;
+console.log(b(1, 2)); // 3
+
+// Example 3
+const e = (f, g) => ({
+    f,
+    g,
+    total: f + g
+});
+console.log(e(2, 3)); // 5
+
+// Example 4
+const h = () => {
+    console.log(this); // {} => global object
 };
 
-const b = {
+h();
+
+const i = {
+    foo: "bar"
+}
+
+h.call(i); // {} => global object
+
+// Example 5
+/* Arrow functions */
+const j = {
     foo: "bar",
-    fn() {
-        function c() {
-            console.log(this);
-        }
-        c.call(this); // foo: "bar"
-        c.call(a); // foo: "not bar"
-    }
-};
-b.fn();
-
-const d = {
-    name: "Jean",
-    lastName: "Louis"
-};
-
-function hello(lang) {
-    if (lang === "fr") {
-        console.log(`bonjour: ${ this.name + this.lastName}`);
-    } else {
-        console.log(`hi: ${ this.name + this.lastName}`);
+    fn: () => {
+        console.log(this); // {} => global object
     }
 }
 
-hello.call(d, "fr"); // bonjour: JeanLouis
-hello.apply(d, ["fr"]); // bonjour: JeanLouis
+j.fn(); // {} => global object
 
-const e = hello.bind(d);
-e("fr"); // bonjour: JeanLouis
+/* Not arrow functions */
+const k = {
+    foo: "bar",
+    fn() {
+        const fn2 = () => {
+            console.log(this); // {} => global object
+        };
+        // console.log(this); // {} => global object
+        fn2();
+    }
+}
+
+k.fn(); // {foo: "bar", fn: f} => global object
