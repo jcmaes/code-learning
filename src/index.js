c3ae0d3023894ecf0f5e756f17fe611b693c5aac
Jean-Christophe Maes <jc.maes@wavenet.be>
2022-12-07 16:39:05 +0100

    Lesson 54 ending.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-/* Keyword this */
+/* Set or link this */
 
-import "./test";
+const a = {
+    foo: "not bar"
+};
 
-function func() {
-    console.log(this);
+const b = {
+    foo: "bar",
+    fn() {
+        function c() {
+            console.log(this);
+        }
+        c.call(this); // foo: "bar"
+        c.call(a); // foo: "not bar"
+    }
+};
+b.fn();
+
+const d = {
+    name: "Jean",
+    lastName: "Louis"
+};
+
+function hello(lang) {
+    if (lang === "fr") {
+        console.log(`bonjour: ${ this.name + this.lastName}`);
+    } else {
+        console.log(`hi: ${ this.name + this.lastName}`);
+    }
 }
 
-func();
+hello.call(d, "fr"); // bonjour: JeanLouis
+hello.apply(d, ["fr"]); // bonjour: JeanLouis
+
+const e = hello.bind(d);
+e("fr"); // bonjour: JeanLouis
