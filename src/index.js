7f60c704bdc936fb50569ba175292a80d901e7c7
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-09-25 20:44:51 +0200

    Lesson 58 ending.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-/* Nesting array */
+/* Introduction to array  */
 
-const array = [["earth", "moon"], "sun", ];
-console.log(array[0]); // "earth, moon"
-console.log(array); // Array(2), "sun"
-console.log(array[0][0]); // "earth"
-console.log(array[0][array[0].length - 1]); // "moon"
+// Notation literal
+const array = [1, "two", {foo: "bar"}, [1, 2, 3]];
+console.log(array);
+
+// Function
+const array2 = Array(1, 2, 3);
+console.log(array2);
+
+// Constructor
+const array3 = new Array(2, 3, 4);
+console.log(array3);
+
+// Static methode
+const array4 = Array.from("Hello");
+console.log(array4);
+
+const array5 = Array.from([10, 20 ,30], number => number + 1);
+console.log(array5);
+
+// Check if element is an array
+console.log(Array.isArray(array))
