defe683da1e3f5964c3b16abef49c6f622bf2522
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-09-29 00:38:53 +0200

    Lesson 77 ending.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-/* Re-export and dynamic imports */
+/* Select DOM elements */
 
-import './app/app.js';
+console.log(document);
+console.log(window);
+
+// Old way
+const elem1 = document.getElementById('foo');
+console.log(elem1) // <img>
+
+const elem2 = document.getElementsByClassName('class-test');
+console.log(elem2) // <p>
+
+// New way
+const elem3 = document.querySelector('section')
+console.log(elem3) // <section>
+
+const elem4 = document.querySelectorAll('img')
+console.log(elem4) // <img>
