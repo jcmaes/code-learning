02b24a5b3df88625e1d1a2bdc9cabc815700c255
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-09-28 22:57:28 +0200

    Lesson 72 ending.

--- src/lib1.js
+++
@@ -1,9 +0,0 @@
-const lib1 = (function() {
-    function sayHello() {
-        console.log('Hello');
-    }
-
-    return {
-        sayHello
-    }
-})();
--- src/main.js
+++
@@ -1,9 +0,0 @@
-function sayHello() {
-    console.log('Main');
-}
-
-sayHello();
-
-console.log(lib1);
-
-lib1.sayHello();
