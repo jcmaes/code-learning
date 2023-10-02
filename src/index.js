c85da76cd11bf55a07a1b561ca22b0dcdc9e89aa
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-02 21:56:59 +0200

    Lesson 84 ending.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* Create nodes */
+/* Positioning nodes */
 
 import './style.css';
 
@@ -10,14 +10,29 @@ const image = document.createElement("img");
 const imageSrc = document.createAttribute("src");
 imageSrc.value = "https://picsum.photos/seed/picsum/400/300";
 
+image.setAttributeNode(imageSrc);
+
 const paragraph = document.createElement("p");
 const paragraphClass = document.createAttribute("class");
 paragraphClass.value = "text-primary";
 const paragraphText = document.createTextNode("I'm a paragraph");
+
+paragraph.setAttributeNode(paragraphClass);
 
 const link = document.createElement("a");
 const linkHref = document.createAttribute("href");
 linkHref.value = "https://dyma.fr";
 const linkText = document.createTextNode("Dyma");
 
+link.setAttributeNode(linkHref);
+
 const comment = document.createComment("I am a comment");
+
+app.appendChild(section);
+section.append(image);
+paragraph.append(paragraphText, link);
+link.appendChild(linkText);
+
+paragraph.insertBefore(comment, link);
+
+section.insertAdjacentElement("beforeend", paragraph);
