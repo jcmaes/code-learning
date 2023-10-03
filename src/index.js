f2db09b1ac11040626249f2feefafb04417faf00
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-03 21:43:33 +0200

    Lesson 89 ending.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-/* AddEventListener() method */
+/* Delete an event handler and trigger an event */
 
 import './style.css';
 
 const square = document.querySelector("div");
+const button = document.querySelector("button");
 
-square.addEventListener('mouseover', () => {
+function becomeRed() {
     square.style.backgroundColor = "red";
-}, {
-    once: true
-});
+}
 
-square.addEventListener('mouseover', () => {
-    square.style.width = "300px";
-    square.style.height = "300px";
-});
+square.addEventListener('mouseover', becomeRed);
 
 square.addEventListener('mouseout', () => {
     square.style.backgroundColor = "#eee";
+    square.removeEventListener('mouseover', becomeRed);
 });
 
-square.addEventListener('mouseout', () => {
-    square.style.width = "200px";
-    square.style.height = "200px";
+button.addEventListener('click', () => {
+    square.dispatchEvent(new Event('mouseover'));
 });
