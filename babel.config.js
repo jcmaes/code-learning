e2d578ad0b4d5f063a3b363c4332d228b6d47abc
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-05 10:37:59 +0200

    Lesson 102 ending.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     presets: [
         [
-            '@babel/preset-env'
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: 3,
+                debug: true,
+                targets: '> 30%, not dead'
+            }
         ]
     ]
-}
+};
