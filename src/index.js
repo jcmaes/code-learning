77406a8df545343f340c409d9e0e56722b16acef
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-05 22:37:39 +0200

    Lesson 113 ending.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
-/* Web API URL */
+/* XMLHttpRequest */
 
-const url = new URL('https://wikipedia.fr/napoleon?key1=value1&key2=value2');
+// Method GET
+const xhr = new XMLHttpRequest();
 
-url.searchParams.append('key3', 'value3');
-url.searchParams.set('key4', 'tom&jerry');
+xhr.open("GET", 'https://jsonplaholder.typicode.com/todos');
+xhr.responseType = 'json';
+xhr.send();
 
-console.log(URL.toString());
+xhr.addEventListener('load', response => {
+    console.log(response);
+    const result = xhr.response;
+    console.log(xhr.response);
+});
+
+xhr.addEventListener('error', error => {
+    console.log(error);
+});
+
+xhr.addEventListener('progress', progress => {
+    console.log(progress);
+});
+
+// Method POST
+const todo = {
+    userId: 1000,
+    title: "hello",
+    complete: false
+}
+const xhr2 = new XMLHttpRequest();
+
+xhr2.open("POST", 'https://jsonplaholder.typicode.com/todos');
+xhr2.responseType = 'json';
+xhr2.setRequestHeader('Content-type', 'application/json')
+xhr2.send();
+
+xhr2.addEventListener('load', response => {
+    console.log(response);
+    console.log(xhr2);
+    const result = xhr2.response;
+    console.log(xhr2.response);
+});
+
+xhr2.addEventListener('error', error => {
+    console.log(error);
+});
+
+xhr2.addEventListener('progress', progress => {
+    console.log(progress);
+});
+
+xhr2.addEventListener('readystatechange', event => {
+    console.log(event);
+    console.log(xhr2.readyState);
+});
