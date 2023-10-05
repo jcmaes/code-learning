29a037ca2f84c44ebec41843ba1ce00b005351fe
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-05 23:08:48 +0200

    Lesson 114 ending.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* XMLHttpRequest */
+/* Advanced XMLHttpRequest uses */
 
 // Method GET
 const xhr = new XMLHttpRequest();
@@ -30,9 +30,15 @@ const todo = {
 const xhr2 = new XMLHttpRequest();
 
 xhr2.open("POST", 'https://jsonplaholder.typicode.com/todos');
+xhr2.timeout = 1000;
 xhr2.responseType = 'json';
 xhr2.setRequestHeader('Content-type', 'application/json')
-xhr2.send();
+xhr2.send(JSON.stringify(todo));
+
+// xhr2.addEventListener('abort', event => console.log(event));
+// xhr2.abort();
+
+xhr2.addEventListener('timeout', event => console.log(event));
 
 xhr2.addEventListener('load', response => {
     console.log(response);
@@ -52,4 +58,26 @@ xhr2.addEventListener('progress', progress => {
 xhr2.addEventListener('readystatechange', event => {
     console.log(event);
     console.log(xhr2.readyState);
+});
+
+// Progress
+const xhr3 = new XMLHttpRequest();
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    xhr3.open("POST", 'https://restapi.fr/upload');
+
+    xhr3.upload.addEventListener('loadstart', () => console.log('load started'));
+    xhr3.upload.addEventListener('progress', event => {
+        console.log(event);
+        const percentage = ((event.loaded / event.total) * 100).toFixed(0);
+        console.log(percentage, '%');
+    });
+    xhr3.upload.addEventListener('loadend', () => console.log('load ended'));
+    xhr3.upload.addEventListener('error', () => console.log('error'));
+
+    xhr3.send(formData);
 });
