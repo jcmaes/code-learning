8cbc796c6c997a8d08af60232de5921a36fa4a8c
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-10 00:21:13 +0200

    Lesson 127 ending.

--- src/assets/javascripts/topbar.js
+++ src/assets/javascripts/topbar.js
@@ -1,1 +1,54 @@
-console.log('topbar');
+const iconMobile = document.querySelector('.header-menu-icon');
+const headerMenu = document.querySelector('.header-menu');
+let isMenuOpen = false;
+let mobileMenuDOM;
+
+const closeMenu = () => {
+    mobileMenuDOM.classList.remove('is-open');
+};
+
+const createMobileMenu = () => {
+    mobileMenuDOM =document.createElement('div');
+    mobileMenuDOM.classList.add('mobile-menu');
+    mobileMenuDOM.addEventListener('click', event => {
+        event.stopPropagation();
+    });
+    mobileMenuDOM.append(headerMenu.querySelector('ul').cloneNode(true));
+    headerMenu.append(mobileMenuDOM);
+};
+
+const openMenu = () => {
+    if(mobileMenuDOM) {
+    } else {
+        createMobileMenu();
+    }
+    mobileMenuDOM.classList.add('is-open');
+};
+
+
+const toggleMobileMenu = event => {
+    // console.log(event);
+    if(isMenuOpen) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+    isMenuOpen = !isMenuOpen;
+};
+
+iconMobile.addEventListener('click', event => {
+    event.stopPropagation();
+    toggleMobileMenu();
+});
+
+window.addEventListener('click', () => {
+    if(isMenuOpen) {
+        toggleMobileMenu();
+    }
+});
+
+window.addEventListener('resize', event => {
+    if(innerWidth > 480 && isMenuOpen) {
+        toggleMobileMenu();
+    }
+});
