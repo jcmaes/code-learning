7e304bcca5e8151b279301e36c28002af8bc8ba7
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-09 23:14:56 +0200

    Lesson 126 ending.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.scss';
 const articleContainerElement = document.querySelector('.article-container');
 
 const createArticles = (articles) => {
-  const articlesDOM = articles.map(article => {
-     const articleDom = document.createElement('div');
-     articleDom.classList.add('article');
-     articleDom.innerHTML = `
+    const articlesDOM = articles.map(article => {
+        const articleDom = document.createElement('div');
+        articleDom.classList.add('article');
+        articleDom.innerHTML = `
 <img src="${article.image}" alt="profile">
 <h2>${article.title}</h2>
 <p class="article-author">${article.author} - ${article.category}</p>
@@ -16,17 +16,35 @@ const createArticles = (articles) => {
     <button class="btn btn-danger" data-id="${article._id}">Delete</button>
 </div>
 `;
-     return articleDom;
-  });
+        return articleDom;
+    });
     // console.log(articlesDOM);
     articleContainerElement.innerHTML = '';
     articleContainerElement.append(...articlesDOM);
+    const deleteButtons = articleContainerElement.querySelectorAll('.btn-danger');
+    // console.log(deleteButtons);
+    deleteButtons.forEach( button => {
+        button.addEventListener('click', async event => {
+            try {
+                const target = event.target;
+                const articleId = target.dataset.id;
+                const response = await fetch(`https://restapi.fr/api/articles/${articleId}`, {
+                    method: "DELETE"
+                });
+                const body = await response.json();
+                // console.log(body);
+                fetchArticle();
+            } catch (e) {
+                console.log('e: ', e);
+            }
+        });
+    });
 };
 const fetchArticle = async () => {
     try {
         const response = await fetch("https://restapi.fr/api/articles");
         const articles = await response.json();
-        console.log(articles);
+        // console.log(articles);
         createArticles(articles);
     } catch (e) {
         console.log("e:", e);
