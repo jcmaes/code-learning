4ef8ac56b8ee9462bb8f3a999e3be359f077a7e1
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-12 21:07:06 +0200

    Lesson 131 ending.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-const date = new Date("2020-01-07T09:50:02.208Z");
-console.log(date.toISOString());
+const date = new Date();
 
-const today = Date.parse("2023-10-12T18:45:14.507Z"); // timestamp
-console.log(today); // 1697136314507
+console.log(date.getDate()); // 12
+console.log(date.getDay()); // 4
+console.log(date.getFullYear()); // 2023
+console.log(date.getHours()); // 20
+console.log(date.getUTCMinutes()); // 59
 
-const todayTimestamp = Date.now(); // timestamp
-
-console.log(todayTimestamp - date); // 118745968859
-console.log(todayTimestamp - date.getTime()); // 118745968859
-
-// Convert to minutes
-console.log((todayTimestamp - date) / 1000 / 60); // 1979101.0796166665
-
+// Increment date
 const date2 = new Date();
-let date3;
-setTimeout(() => {
-    date3 = new Date();
-    const diff = date2 - date;
-    console.log(diff); // 3000
-}, 3000);
+date2.setHours(date2.getHours() + 1);
+
+console.log(date2); // Thu Oct 12 2023 22:02:09 GMT+0200 (Central European Summer Time)
