1ad7d5cda5f297dd108be72b25b10e574e04e1af
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-29 21:41:57 +0100

    Lesson 149 ending.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 // Example 1
-const engine = {
-  power: 240,
-};
-
 const vehicle = {
   hasEngine: true,
   start() {
@@ -10,44 +6,12 @@ const vehicle = {
   },
 };
 
-vehicle.__proto__ = engine;
-
-const bus = {
-  brand: "volvo",
-};
-
-bus.__proto__ = vehicle;
-
-const car = {
-  brand: "renault",
-};
-
-car.__proto__ = vehicle;
-
-console.log(car);
-console.log(car.hasEngine); // true
-car.start(); // vroum !
-console.log(vehicle.power); // 240
-console.log(car.power); // 240
-
-// Example 2
-const bicycle = {
-  hasAssistance: true,
-  go() {
-    console.log(this.hasAssistance);
-    console.log("go !");
-  },
-};
-
-const model = {
-  brand: "Trek",
-};
-
-model.__proto__ = bicycle;
-
-model.hasAssistance = false;
-
-for (let key in model) {
-  console.log(model.hasOwnProperty(key));
-  console.log(key);
+function Car(brand) {
+  this.brand = brand;
 }
+
+Car.prototype = vehicle;
+
+const myCar = new Car("renault");
+
+console.log(myCar);
