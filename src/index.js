eb38d35fa942cd787cfa3f6583431ccba5426d0f
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-29 20:32:51 +0100

    Lesson 146 ending.

--- src/index.js
+++ src/index.js
@@ -1,60 +1,48 @@
 // Method 1
-const obj = {
-  key: "value",
+const user = {
+  firstname: "Tintin",
+  lastname: "Milou",
+  fullname: function () {
+    return this.firstname + " " + this.lastname;
+  },
 };
 
-obj.key = "value2";
-
-console.log(obj); // {key: "value2"}
-
-for (let key in obj) {
-  console.log(obj); // key
-}
+console.log(user.fullname());
 
 // Method 2
-const obj2 = {
-  key: "newValue",
+const user2 = {
+  firstname: "Bob",
+  lastname: "Bobette",
+  get fullname() {
+    return this.firstname + " " + this.lastname;
+  },
+  set fullname(value) {
+    [this.firstname, this.lastname] = value.split(" ");
+  },
 };
 
-obj2.key = "newValue2";
+user.fullname = "foo bar";
 
-console.log(Object.keys(obj2)); // ["key"]
-console.log(Object.values(obj2)); // ["newValue2"]
-console.log(Object.entries(obj2)); // [Array(2)] ["key", "newValue2"]
+console.log(user2.fullname);
 
 // Method 3
-const obj3 = {
-  key: "anotherNewValue",
+const user3 = {
+  firstname: "Tom",
+  lastname: "Jerry",
 };
 
-obj3.key = "anotherNewValue3";
-
-const descriptor = Object.getOwnPropertyDescriptor(obj, "key");
-
-console.log(descriptor); // {value: "", writable: true, enumerable: true, configurable: true}
-
-// Property by property
-Object.defineProperty(obj3, "key", {
-  writable: false,
-});
-
-// Method 4
-const obj4 = {
-  key: "value",
-};
-
-obj4.key = "value4";
-
-const descriptors = Object.getOwnPropertyDescriptors(obj4);
-
-// All properties in the same time
-Object.defineProperties(obj4, {
-  test: {
-    value: "newValue4",
-    writable: true,
-    enumerable: true,
-    configurable: true,
+Object.defineProperty(user, "fullname", {
+  get: function () {
+    return this.firstname + " " + this.lastname;
   },
+  set() {
+    [this.firstname, this.lastname] = value.split(" ");
+  },
+  enumerable: true,
+  writable: true,
 });
 
-console.log(descriptors); // {key: {}}
+const descriptor = Object.getOwnPropertyDescriptor(user, fullname);
+
+console.log(descriptor);
+console.log(user.fullname);
