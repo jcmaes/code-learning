5d04b7697d061cd864012f47c5060302cdd9cf46
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-30 21:12:33 +0100

    Lesson 158 ending.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 // Example 1
-class ExtendedArray extends Array {
-  random() {
-    const length = this.length;
-    let i = 0;
+class Car {}
 
-    while (i < length) {
-      this[i] = Math.round(Math.random() * 10);
-      i++;
-    }
-  }
-}
+const car = new car();
 
-const array = new ExtendedArray(1, 2, 3);
-
-array.random();
-console.log(array);
+console.log(car instanceof Object); // true
