208390155f118adbadcbdd9a17d7226e028ce507
Jean-Christophe Maes <jc.maes@wavenet.be>
2023-10-30 20:45:07 +0100

    Lesson 155 ending.

--- src/index.js
+++ src/index.js
@@ -1,52 +1,14 @@
 // Example 1
-function Vehicle() {
-  this.hasEngine = true;
+class Vehicle {
+  static description() {
+    console.log("use to build a car");
+  }
 }
 
-Vehicle.prototype.stop = function () {
-  console.log("vehicle stop");
-};
-
-function Car() {
-  Vehicle.call(this);
-  this.wheels = 4;
+class Car extends Vehicle {
+  static compareCar(car1, Car2) {}
 }
 
-Car.prototype = Object.create(Vehicle.prototype);
-Car.prototype.constructor = Car;
-Car.prototype.start = function () {
-  console.log("car start");
-};
+// Car.description();
 
 const car = new Car();
-console.log(car); // Car {}
-
-// Example 2
-class Motorcycle {
-  constructor() {
-    this.hasEngine = true;
-  }
-
-  start() {
-    console.log("engine start");
-  }
-
-  stop() {
-    console.log("stop engine");
-  }
-}
-
-class Model extends Motorcycle {
-  constructor() {
-    super();
-    this.wheels = 2;
-  }
-
-  start() {
-    super.start();
-    console.log("start engine");
-  }
-}
-
-const moto = new Model();
-console.log(moto); // Model {}
